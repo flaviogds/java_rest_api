b0cc724fa2b31542489e1fd60d61c7a05e23c3cf
Flávio Santos <flavio.gds@gmail.com>
2021-03-25 22:50:43 -0300

    development of the update and delete method

--- src/main/java/api/rest/personapi/service/PersonService.java
+++ src/main/java/api/rest/personapi/service/PersonService.java
@@ -27,11 +27,8 @@ public class PersonService {
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
 
-        Person saved = personRepository.save(personToSave);
-        return MessageResponseDTO
-                .builder()
-                .message("Created Person With Id " + saved.getId())
-                .build();
+        Person createdPerson = personRepository.save(personToSave);
+        return createMessageResponse(createdPerson.getId(), "Created person With Id ");
     }
 
     public List<PersonDTO> listAll() {
@@ -42,9 +39,35 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = verifyIfExists(id);
 
         return personMapper.toDTO(person);
+    }
+
+    public void deleteById(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        personRepository.deleteById(id);
+    }
+
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        Person personToUpdate = personMapper.toModel(personDTO);
+
+        Person updatedPerson = personRepository.save(personToUpdate);
+        return createMessageResponse(updatedPerson.getId(), "Updated person With Id ");
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String s) {
+        return MessageResponseDTO
+                .builder()
+                .message(s + id)
+                .build();
     }
 }
